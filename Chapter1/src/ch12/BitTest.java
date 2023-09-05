b1940f0f9fec81c8a82764178552c6754df0f70d
moon <anstd9@naver.com>
2023-09-05 16:28:53 +0900

    1. 챕터12에서 9/4 BitTest 빠진내용 추가 2. ConditionTest에서 주석처리했던 scanner부분 주석 해제

---
+++ Chapter1/src/ch12/BitTest.java
@@ -0,0 +1,29 @@
+package ch12;
+
+/*
+ * 2023.09.04
+ * 문서영 실습
+ * Day3
+ * */
+
+public class BitTest {
+
+	public static void main(String[] args) {
+
+		int num1 = 5;  	// 00000101
+		int num2 = 10; 	// 00001010
+				
+		System.out.println(num1 | num2);	//겹치는 애들 00001111 해서 15
+		System.out.println(num1 & num2);	//겹치는 애들이 없어서 00000000
+		System.out.println(num1 ^ num2);	//서로 다른 경우에 1이라서 00001111이라 15
+		System.out.println(~num1);			//반전된 수 출력
+		
+		System.out.println(num1 << 2);		//왼쪽으로 2비트 이동
+		System.out.println(num1);			//num1은 여전히 5를 유지. 값을 바뀌게 하려면
+		System.out.println(num1 <<= 2);		//이동하고 대입해줘야돼.
+		System.out.println(num1);			//그럼 이제 num1의 값은 20으로 바뀌어. 
+
+		
+	}
+
+}
--- Chapter1/src/ch12/ConditionTest.java
+++ Chapter1/src/ch12/ConditionTest.java
@@ -1,6 +1,6 @@
 package ch12;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 /*
  * 2023.09.04
@@ -15,14 +15,14 @@ public class ConditionTest {
 		int max;
 		System.out.println("두 수를 입력 받아서 큰 수를 출력하세요");
 		
-//		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("input1:");
-//		int x = scanner.nextInt();
+		int x = scanner.nextInt();
 		System.out.println("input1:");
-//		int y = scanner.nextInt();
+		int y = scanner.nextInt();
 		
-//		max = (x>y)? x : y ;
-//		System.out.println(max);
+		max = (x>y)? x : y ;
+		System.out.println(max);
 		
 	}
 
