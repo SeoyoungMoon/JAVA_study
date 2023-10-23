eac48e8033e5e45f93416f995be9591feaa13b4b
SeoyoungMoon <'anstd9@naver.com'>
2023-10-23 12:29:27 +0900

    다운 캐스팅과 instanceof

--- Chapter5/src/ch06/AnimalTest.java
+++ Chapter5/src/ch06/AnimalTest.java
@@ -3,9 +3,9 @@ package ch06;
 import java.util.ArrayList;
 
 /*
- * 2023.10.16
+ * 2023.10.16, 10.23
  * 문서영 실습
- * chapter5 45강 <다형성과 다형성을 사용하는 이유> 
+ * chapter5 45강 <다형성과 다형성을 사용하는 이유> , 47강 <다운캐스팅과 instance of>
  * Day19
  * */
 
@@ -26,23 +26,40 @@ class Animal{
 	public void move() {
 		System.out.println("동물이 움직입니다. ");
 	}
+
+	public void eating() {
+		
+	}
+
 }
 
 class Human extends Animal{
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다. ");
 	}
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
+
 }
 
 class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네발로 걷습니다. ");
 	}
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다.");
+	}
+
 }
 
 class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다. ");
+	}
+	
+	public void flying() {
+		System.out.println("독수리가 날개를 펴고 날아갑니다. ");
 	}
 }
 
@@ -67,10 +84,34 @@ public class AnimalTest {
 		animalMove(tAnimal);
 		animalMove(eAnimal);
 
+		AnimalTest test = new AnimalTest();
+		test.testDownCasting(animalList);
 	}
 	
 	public static void animalMove( Animal animal) {
 		animal.move();	
 	}
 
+	public static void testDownCasting(ArrayList<Animal> animalList) {
+		
+		for(int i=0; i<animalList.size(); i++) {
+			
+			Animal animal = animalList.get(i);
+			
+			if(animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			} else if(animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			} else if(animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			} else {
+				System.out.println("error");
+			}
+			
+		}
+		
+	}
 }
